60e4cbaf2dc30d9a530f2819cbdb8bf3b711aa28
kaito nakazaki <kaitonakazaki@gmail.com>
2018-08-22 15:50:39 +0900

    second-commit

--- app/controllers/twets_controller.rb
+++ app/controllers/twets_controller.rb
@@ -1,13 +1,15 @@
 class TwetsController < ApplicationController
   before_action :set_twet, only: [:edit, :update,:destroy]
+  before_action :login_confirm, only: [:new, :edit, :show,:destroy]
+
   def index
     @twets = Twet.all
   end
-  
-  def new 
+
+  def new
    @twet =  Twet.new
   end
-  
+
   def create
     @twet = Twet.create(twet_params)
     if @twet.save
@@ -16,10 +18,10 @@ class TwetsController < ApplicationController
         render 'new'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @twet.update(twet_params)
       redirect_to twets_path, notice: "ブログを編集しました！"
@@ -27,18 +29,24 @@ class TwetsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
       @twet.destroy
       redirect_to twets_path, notice:"ブログを削除しました！"
   end
-  
+
    private
    def twet_params
       params.require(:twet).permit(:content)
    end
-   
+
    def set_twet
       @twet = Twet.find(params[:id])
+   end
+
+   def login_confirm
+      if current_user.nil?
+        redirect_to "/sessions/new"
+      end
    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # get '/',to:'twets#index'
   root 'twets#index'
   get '/home',to:'twets#home'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :twets
+  resources :users
 end
